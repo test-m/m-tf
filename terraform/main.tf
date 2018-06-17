2284d69d4c29dc2385b348aefc299ad512e87039
root <root@ip-172-31-47-198.eu-west-1.compute.internal>
2018-06-16 21:30:32 -0400

    sg.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "my-test-instance" {
-  ami           = "${data.aws_ami.ubuntu.id}"
+  ami           = "${data.aws_ami.centos7.id}"
   instance_type = "t2.small"
   key_name   = "test_mg"
 
@@ -10,7 +10,7 @@ resource "aws_instance" "my-test-instance" {
   security_groups = [
     "${aws_security_group.allow_ssh1.name}",
     "${aws_security_group.allow_outbound1.name}",
-    "${aws_security_group.allow_http1.name}",
+    "${aws_security_group.allow_http8080.name}",
   ]
 }
 
